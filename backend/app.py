eea3b70e8efd71fce0a0ee8a2eed440ae4ef08cb
ghustaff1 <denismosura@gmail.com>
2026-08-18 14:15:15 +0000

    add basic db logic to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,6 @@
+import os
+
+import psycopg2
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +9,17 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "JP News API is working!"}
+
+
+@app.get("/db")
+def database_test():
+    connection = psycopg2.connect(
+        host="db",
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+    )
+
+    connection.close()
+
+    return {"database": "connected"}
